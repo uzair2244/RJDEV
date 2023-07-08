3f2c3a62f059c41b92b97b48ceb4e69cfa7d726e
Muhammad Uzair Shahid Rao <uzirao17@gmail.com>
2023-07-08 13:53:50 +0500

    first commit

---
+++ Components/About.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About (){
+    return(
+        <div className="about--section">
+            <h2>About</h2>
+            <p>I am a MERN developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        </div>
+    )
+}
---
+++ Components/Interest.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interest (){
+    return(
+        <div className="interest--section">
+            <h2>Interest</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    )
+}
